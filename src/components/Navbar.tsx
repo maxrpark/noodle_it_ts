13da309c5b5b980b76626c7c7b0f3da4fcd98323
Maxigong <maxi@nolgong.com>
2022-05-10 09:29:52 +0900

    changes nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,17 +11,11 @@ import styled from 'styled-components';
 import { FaBars } from 'react-icons/fa';
 
 const NavbarUser: React.FC = () => {
-  const { logOutUser, userAuth } = useUserContext();
+  const { userAuth } = useUserContext();
   const { total_items } = useCartContext();
-  const { closeModal, searchUserQuery, query, result, toogleMenu } =
-    useGlobalContext();
+  const { searchUserQuery, query, result, toogleMenu } = useGlobalContext();
 
   const history = useNavigate();
-
-  const handleLogout = () => {
-    logOutUser();
-    closeModal();
-  };
 
   const search = useRef<HTMLInputElement>(null);
 
@@ -43,47 +37,70 @@ const NavbarUser: React.FC = () => {
 
   return (
     <Wrapper>
-      <NavLink to={'/'} className='logo'>
-        Noodle It!
-      </NavLink>
-      <form onSubmit={handleSearch}>
-        <input type='text' ref={search} />
-      </form>
-      <div className='nav-items'>
-        {userAuth ? (
-          <>
-            <button onClick={() => handleLogout()}>Logout</button>
-            <NavLink to={'/dashboard'}>Profile</NavLink>
-          </>
-        ) : (
-          <>
-            <NavLink to={'/login'}>Log in</NavLink>
-          </>
-        )}
+      <div className='navbar'>
+        <NavLink to={'/'} className='logo'>
+          Noodle It!
+        </NavLink>
+        <form onSubmit={handleSearch}>
+          <input type='text' ref={search} />
+        </form>
+        <div className='nav-items'>
+          {userAuth ? (
+            <>
+              <NavLink to={'/dashboard'}>Profile</NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink to={'/login'}>Log in</NavLink>
+            </>
+          )}
 
-        <Link className='cart-icon' to={'/cart'}>
-          <FaShoppingCart />
-          {total_items}
-        </Link>
+          <Link className='cart-icon' to={'/cart'}>
+            <FaShoppingCart />
+            {total_items}
+          </Link>
+          <FaBars onClick={toogleMenu} />
+        </div>
+        {/* <ToogleTheme /> */}
       </div>
-      <FaBars onClick={toogleMenu} />
-      {/* <ToogleTheme /> */}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 70px;
+const Wrapper = styled.nav`
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 70px;
+    padding: 1rem;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
   .nav-items {
     display: flex;
+    gap: 1rem;
+  }
+
+  form {
+    font-family: var(--primary-font-family);
+    margin: 1rem auto;
+    padding: 1rem;
+    display: flex;
+  }
+  input {
+    justify-self: flex-end;
+    /* height: 35px; */
+    width: 100%;
+    font-family: inherit;
+    font-size: 1rem;
   }
 
   @media screen and (min-width: 768px) {
-    .nav-items {
-      display: flex;
+    form {
+      width: 100%;
+      max-width: 300px;
+      margin: 0 0 0 auto;
     }
   }
 `;
